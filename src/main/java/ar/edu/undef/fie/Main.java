1f8beb2f04a8ea6ff76e492d03994777e9eba06f
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-11 08:56:51 -0300

    Actualizar Codigo

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,9 +1,37 @@
+
+/*
+1. Hacer un programa que calcule el promedio de los números pasados por argumento a través de la consola, y lo muestre por la pantalla.
+ */
+
 package ar.edu.undef.fie;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("No proporcionó elementos");
+            return;
+        }
+
+        System.out.println("Ingresó los siguientes argumentos: ");
+        for (String argumento : args) {
+            System.out.print(argumento + " ");
+        }
+
+        int cantidad = 0;
+
+        int suma = 0;
+
+        for (String argumento : args){
+            int numero = Integer.parseInt(argumento);
+            cantidad++;
+            suma += numero;
+        }
+        float promedio = (float) suma / cantidad;
+
+        System.out.println(" ");
+        System.out.println("El promedio de los valores ingresados como argumentos es de: " + promedio);
     }
 }
+
 
